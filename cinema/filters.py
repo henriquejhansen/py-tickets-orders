772ef9815a1f048598bf5bf1983c129adc6bb295
henriquejhansen <henriquejhansen@gmail.com>
2025-11-07 20:41:43 -0300

    Solution

--- cinema/filters.py
+++ cinema/filters.py
@@ -2,14 +2,14 @@ import django_filters
 from .models import Movie, MovieSession
 
 
+class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
+    pass
+
+
 class MovieFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
-    genres = django_filters.ModelMultipleChoiceFilter(
-        field_name="genres", queryset=Movie.objects.none()
-    )
-    actors = django_filters.ModelMultipleChoiceFilter(
-        field_name="actors", queryset=Movie.objects.none()
-    )
+    genres = NumberInFilter(field_name="genres__id", lookup_expr="in")
+    actors = NumberInFilter(field_name="actors__id", lookup_expr="in")
 
     class Meta:
         model = Movie
@@ -18,7 +18,8 @@ class MovieFilter(django_filters.FilterSet):
 
 class MovieSessionFilter(django_filters.FilterSet):
     movie = django_filters.NumberFilter(field_name="movie_id")
-    show_time = django_filters.DateFilter(field_name="show_time", lookup_expr="date")
+    date = django_filters.DateFilter(field_name="show_time__date")
+
 
     class Meta:
         model = MovieSession
--- cinema/views.py
+++ cinema/views.py
@@ -1,7 +1,18 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticatedOrReadOnly,
+)
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
+
+from .models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order,
+)
 from .serializers import (
     GenreSerializer,
     ActorSerializer,
@@ -20,29 +31,29 @@ from .filters import MovieFilter, MovieSessionFilter
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all().order_by("id")
     serializer_class = GenreSerializer
-    permission_classes = [AllowAny]
+    permission_classes = (AllowAny,)
     pagination_class = None
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all().order_by("id")
     serializer_class = ActorSerializer
-    permission_classes = [AllowAny]
+    permission_classes = (AllowAny,)
     pagination_class = None
 
 
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all().order_by("id")
     serializer_class = CinemaHallSerializer
-    permission_classes = [AllowAny]
+    permission_classes = (AllowAny,)
     pagination_class = None
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all().order_by("id")
-    permission_classes = [AllowAny]
+    permission_classes = (AllowAny,)
     pagination_class = None
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = MovieFilter
 
     def get_serializer_class(self):
@@ -55,9 +66,9 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all().order_by("id")
-    permission_classes = [AllowAny]
+    permission_classes = (AllowAny,)
     pagination_class = None
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = MovieSessionFilter
 
     def get_serializer_class(self):
@@ -70,7 +81,7 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
 
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user).order_by("-created_at")
